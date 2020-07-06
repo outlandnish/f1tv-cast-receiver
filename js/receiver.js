fa2226ed67b6872b4c374b2fa8fce11686fb0d41
Nishanth Samala <nishanth.samala@gmail.com>
2020-07-06 14:10:05 -0400

    basic casting works

--- js/receiver.js
+++ js/receiver.js
@@ -1,53 +1,59 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 const castOptions = new cast.framework.CastReceiverOptions();
-
 let playbackConfig = (Object.assign(new cast.framework.PlaybackConfig(), playerManager.getPlaybackConfig()));
-
-let cookie = null
 
 playerManager.setMessageInterceptor(
   cast.framework.messages.MessageType.LOAD,
   request => {
-    cookie = request.customData
-    document.cookie = cookie
-    // const tokenQueryString = cookie.substring(cookie.indexOf('lvlt_tk_q='), )
-    // request.url += 
+    console.log('loaded', request)
+
+    request.media.hlsSegmentFormat = cast.framework.messages.HlsSegmentFormat.TS
+    setCookies(request.media.customData)
+
     return request;
   });
   
-  playbackConfig.manifestRequestHandler = requestInfo => {
-    requestInfo.withCredentials = true;
-    // requestInfo.headers = { Cookie: cookie }
-    requestInfo.header = { 
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-site'
-    }
-    console.log('manifest request', requestInfo)
-  };
-  
-  playbackConfig.segmentRequestHandler = requestInfo => {
-    requestInfo.withCredentials = true;
-    // requestInfo.headers = { Cookie: cookie }
-    requestInfo.header = { 
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-site'
-    }
-    console.log('segment request', requestInfo)
-  };
-  
-  playbackConfig.licenseRequestHandler = requestInfo => {
-    requestInfo.withCredentials = true;
-    // requestInfo.headers = { Cookie: cookie }
-    requestInfo.header = { 
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-site'
-    }
-    console.log('license request', requestInfo)
-  };
-  
-  castOptions.playbackConfig = playbackConfig;
-  context.start(castOptions);
+playbackConfig.manifestRequestHandler = requestInfo => {
+  requestInfo.withCredentials = true
+  requestInfo.header = { 
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'x-countrycode': 'zero'
+  }
+  console.log('manifest request', requestInfo)
+}
+
+playbackConfig.segmentRequestHandler = requestInfo => {
+  requestInfo.withCredentials = true
+  requestInfo.header = { 
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'x-countrycode': 'zero'
+  }
+  console.log('segment request', requestInfo)
+}
+
+playbackConfig.licenseRequestHandler = requestInfo => {
+  requestInfo.withCredentials = true;
+  requestInfo.header = { 
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'x-countrycode': 'zero'
+  }
+  console.log('license request', requestInfo)
+}
+
+castOptions.playbackConfig = playbackConfig;
+context.start(castOptions)
+
+function setCookies(cookie) {
+  let cookies = cookie.split(';')
+  for (let part of cookies) {
+    console.log('setting cookie', part)
+    document.cookie = part
+  }
+}
